2873c96b5c694747f1506923c57fd1635c69638e
Frank Bruno <frankbrunoc@gmail.com>
2022-03-02 18:36:49 -0300

    Example 1001 and 1002

---
+++ bee-crowd/beginner/1001.js
@@ -0,0 +1,2 @@
+let [A,B] = lines.map(item => parseInt(item))
+console.log("X = "+(A+B))
---
+++ bee-crowd/beginner/1002.js
@@ -0,0 +1,6 @@
+
+
+const pi = 3.14159
+const [R] = lines.map(item => parseFloat(item))
+const A = pi*R**2
+console.log("A=" + A.toFixed(4))
